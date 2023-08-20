2c0e820c1f6cce75f33f1d6a1c8b4cf1949a0ff7
Fahad <205fahadmohammad@gmail.com>
2023-08-20 10:17:22 +0600

    create semester done

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterService } from './academicSemester.service';
+
+const insertIntoDB = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await AcademicSemesterService.insertIntoDB(req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Academic Semester Created!!',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const AcademicSemesterController = {
+  insertIntoDB,
+};
