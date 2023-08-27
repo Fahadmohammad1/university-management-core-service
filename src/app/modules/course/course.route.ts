c90d05f28f4435346885c6b994c2fcfe1f427c10
Fahad <205fahadmohammad@gmail.com>
2023-08-27 23:10:26 +0600

    course crud done

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -7,11 +7,27 @@ import { CourseValidation } from './course.validation';
 
 const router = express.Router();
 
+router.get('/', CourseController.getAllFromDB);
+router.get('/:id', CourseController.getByIdFromDB);
+
 router.post(
   '/',
   validateRequest(CourseValidation.create),
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   CourseController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(CourseValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.deleteByIdFromDB
 );
 
 export const CourseRoute = router;
