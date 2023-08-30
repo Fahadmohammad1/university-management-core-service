38b6931aac7746624805c244ba6faf132035450d
Fahad <205fahadmohammad@gmail.com>
2023-08-30 17:26:53 +0600

    offered course crud done

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -7,6 +7,9 @@ import { OfferedCourseValidations } from './offeredCourse.validation';
 
 const router = express.Router();
 
+router.get('/', OfferedCourseController.getAllFromDB);
+router.get('/:id', OfferedCourseController.getByIdFromDB);
+
 router.post(
   '/',
   validateRequest(OfferedCourseValidations.create),
@@ -14,4 +17,16 @@ router.post(
   OfferedCourseController.insertIntoDB
 );
 
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidations.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseController.deleteByIdFromDB
+);
 export const OfferedCourseRoutes = router;
